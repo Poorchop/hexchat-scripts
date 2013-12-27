e1a548c933cc168327674ccdea3f510b5f4c344d
PoorDog <poordog@openmailbox.org>
2013-12-27 09:17:22 -0500

    Create follow.py

---
+++ follow/README.md
@@ -0,0 +1,12 @@
+Follow
+------
+Format messages from specific users to make them easier to follow
+
+Usage:
+------
+/follow [nick]
+/unfollow [nick]
+
+Notes:
+------
+Bunch of code taken from [TingPing's] (https://github.com/TingPing) scripts
---
+++ follow/follow.py
@@ -0,0 +1,45 @@
+import hexchat
+
+__module_name__ = "Follow"
+__module_author__ = "PDog"
+__module_version__ = "0.0.1"
+__module_description__ = "Format messages from specific users to make them easier to follow"
+
+userlist = []
+edited = False
+
+def follow_cb(word, word_eol, userdata):
+	global userlist
+	
+	if len(word) >= 2 and word[1] not in userlist:
+		userlist.append(word[1])
+		hexchat.prnt("-Follow-\tYou are now following {0}".format(word[1]))
+		return hexchat.EAT_ALL
+
+def unfollow_cb(word, word_eol, userdata):
+	global userlist
+	
+	if len(word) >= 2 and word[1] in userlist:
+		userlist.remove(word[1])
+		hexchat.prnt("-Follow-\tYou have unfollowed {0}".format(word[1]))
+		return hexchat.EAT_ALL
+
+def format_cb(word, word_eol, userdata):
+	global userlist
+	global edited
+	if edited:
+		return
+	
+	if word[0] in userlist:
+		formatted_nick = "\002{0}\002".format(word[0])
+		formatted_msg = "\00318{0}".format(word[1])
+		
+		edited = True
+		hexchat.emit_print(userdata, formatted_nick, formatted_msg)
+		edited = False
+		
+		return hexchat.EAT_ALL
+	
+hexchat.hook_command("FOLLOW", follow_cb)
+hexchat.hook_command("UNFOLLOW", unfollow_cb)
+hexchat.hook_print("Channel Message", format_cb, "Channel Message")
