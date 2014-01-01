82d369e247a4b503f8594bde9e95e3cc7108cdf1
PoorDog <poordog@openmailbox.org>
2014-01-01 12:46:22 -0500

    Update adfilter.py

--- adfilter/README.md
+++ adfilter/README.md
@@ -14,4 +14,4 @@ The current filters only include ads that I have encountered thus far.
 If you find an ad that you would like me to add to the list, please let me know and send me the entire content of the ad,
 preferably with all color and attribute formatting stripped from it.
 
-TODO: Add filter for fserve.pl (irssi), add filter for Obsidian², add filter for Iterati Solutions fserve, also filter CTCPs from fserves, improve regexes
+TODO: Add filter for fserve.pl (irssi), add filter for Obsidian², add filter for Iterati Solutions fserve, filters for various unknown fserves, improve regexes
--- adfilter/adfilter.py
+++ adfilter/adfilter.py
@@ -3,7 +3,7 @@ import re
 
 __module_name__ = "AdFilter"
 __module_author__ = "PDog"
-__module_version__ = "0.1.1"
+__module_version__ = "0.2"
 __module_description__ = "Move fserve advertisements to a separate tab"
 
 # Add channels from which you would like to filter ads, e.g. channels = ["#freenode", "#defocus", "##linux"]
@@ -12,10 +12,15 @@ channels = []
 # Customize the name of the tab to your liking
 tab_name = "(Ads)"
 
-omenserve_regex = re.compile(".*?Type:\s+\W[\w-]+\s+For\sMy\sList\sOf:\s+[\d,]+\s+Files\s+.*?Slots:\s+\d+/\d+\s+.*?Queued:\s+\d+\s+.*?Speed:\s+[\d,]+cps\s+.*?Next:\s+\w+\s+.*?Served:\s+[\d,]+\s+.*?List:\s+[A-Z][a-z]+\s+\w+\s+.*?Search:\s+[A-Z]{2,3}\s+.*?Mode:\s+\w+\s+.*?$")
 bwi_regex = re.compile("^(\[BWI\])\sType\s+\W[\w-]+\s+to\sget\sthe\slist\sof\s+[\d,]+\s+files\s\([\d\.]+\s+[A-Z]+\)\.\s+Updated\son\s+[\d+-]+\s+[\d:]+\s+Total\sSent\(channel\):\s+[\d,]+\s+\([\d\.]+\s+[A-Z]+(\))$")
+irssi_fserve_regex = re.compile("^(\(FServe Online\))\s+Note:\(Type\s+\W[\w-]+\s+for\s+filelist\)\s+Trigger:\(/ctcp\s+.*?\)\s+On\s+FServe:\(.*?\)\s+Sends:\(")
+omenserve_regex = re.compile(".*?Type:\s+\W[\w-]+\s+For\sMy\sList\sOf:\s+[\d,]+\s+Files\s+.*?Slots:\s+\d+/\d+\s+.*?Queued:\s+\d+\s+.*?Speed:\s+[\d,]+cps\s+.*?Next:\s+\w+\s+.*?Served:\s+[\d,]+\s+.*?List:\s+[A-Z][a-z]+\s+\w+\s+.*?Search:\s+[A-Z]{2,3}\s+.*?Mode:\s+\w+\s+.*?$")
+unknown_one_regex = re.compile("^(Type)\s+\W[\w-]+\s+for\smy\slist\sof\s+\([\d,]+\)\s+Ebooks\screated\son\s+[\d-]+\s+([\d:]+)$")
 
-ad_lst = [omenserve_regex, bwi_regex]
+ad_lst = [bwi_regex, irssi_fserve_regex, omenserve_regex, unknown_one_regex]
+server_nicks = []
+
+moved = False
 
 def find_adtab():
 	context = hexchat.find_context(channel=tab_name)
@@ -31,15 +36,37 @@ def adfilter_cb(word, word_eol, userdata):
 
 	global channels
 	global ad_lst
+	global server_nicks
 	channel = hexchat.get_info("channel")
 	stripped_msg = hexchat.strip(word[1], -1, 3)
 	
 	for ad in ad_lst:
 		if ad.match(stripped_msg) and channel in channels:
+		
+			if word[0] not in server_nicks:
+				server_nicks.append(word[0])
+				
 			ad_context = find_adtab()
 			ad_context.prnt("{0}\t\00318<{4}{3}{1}>\00399 {2}".format(channel, *word))
 			return hexchat.EAT_ALL
-			
+
+def ctcpfilter_cb(word, word_eol, userdata):
+	global moved
+	global channels
+	global server_nicks
+	
+	if moved:
+		return
+	
+	if word[1] in server_nicks and word[2] in channels:
+		ad_context = find_adtab()
+		
+		moved = True
+		ad_context.emit_print("CTCP Generic to Channel", *word)
+		moved = False
+		
+		return hexchat.EAT_ALL
+
 def unload_cb(userdata):
 	for chan in hexchat.get_list("channels"):
 		if chan.type == 1 and chan.channel == tab_name:
@@ -48,6 +75,7 @@ def unload_cb(userdata):
 	hexchat.prnt(__module_name__ + " version " + __module_version__ + " unloaded")
 
 hexchat.hook_print("Channel Message", adfilter_cb)
+hexchat.hook_print("CTCP Generic to Channel", ctcpfilter_cb)
 hexchat.hook_unload(unload_cb)
 
 hexchat.prnt(__module_name__ + " version " + __module_version__ + " loaded")
