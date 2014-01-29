57841449e698e2595dc667cae6337f28caa43bea
PoorDog <poordog@openmailbox.org>
2014-01-29 16:23:00 -0500

    Remove unrelated scripts

--- random/build-scripts/bitcoin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`/../bitcoin/ &&
-git clean -xfd &&
-git reset --hard origin/master &&
-git merge theuni/qt5-build &&
-./autogen.sh &&
-./configure --with-qt=qt5 --disable-tests &&
-make -j2
