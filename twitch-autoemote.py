c2c85d1ba2678f908059c7dc2049bbb46fade44b
Zooback <poordog@openmailbox.org>
2014-02-14 20:58:08 -0500

    Update twitch-autoemote.py
    -Gave script a proper GNOME dev treatment and deleted everything

--- twitch-autoemote.py
+++ twitch-autoemote.py
@@ -1,26 +1,10 @@
 import hexchat
 
 __module_name__ = "Twitch Emote Autoformat"
-__module_author__ = "PoorDog"
-__module_version__ = "0.3.2"
+__module_author__ = "PDog"
+__module_version__ = "0.4"
 __module_description__ = "Automatically format twitch.tv emote names with proper capitalization"
 
-hexchat.prnt (__module_name__ + " version " + __module_version__ + " loaded.")
-
-twitch_help = "Twitch: To add an emote for current session:\n \
-  /twitch add <alias> <emote>\n \
-  Ex: /twitch add frankerz FrankerZ\n \
-  To remove an alias for current session:\n \
-  /twitch delete <alias>\n \
-  To list your current emotes:\n \
-  /twitch emotes\n \
-  To list your current aliases:\n \
-  /twitch aliases\n \
-  To check the emote associated with an alias:\n \
-  /twitch output <alias>"
-  # add info for mod commands when combining with TingPing's twitch.py script
-
-# emote_dict is customizable. You can easily add or remove rules to your liking. Subscriber-only emotes are not included in the default list, so these can appended as necessary.
 # emote names taken from: http://twitchemotes.com/
 # list last updated Sept 20, 2013
 emote_dict = {'4head' : '4Head', 
@@ -106,74 +90,33 @@ emote_dict = {'4head' : '4Head',
               'wholewheat' : 'WholeWheat', 
               'winwaker' : 'WinWaker'}
 
-unedited = True
+moved = False
 
 def is_twitch():
-    if "twitch.tv" in hexchat.get_info("server"):
+    if "twitch.tv" in hexchat.get_info("host"):
         return True
     else: 
         return False
-
-def twitch_cb(word, word_eol, userdata):
-    if len(word) > 1:
-        cmd = word[1].lower()
-
-        if cmd == "add" and len(word) == 4:
-            emote_dict.update({word[2] : word[3]})
-            hexchat.prnt("*\t\00319\002{0}\002\00399 with alias \00320\002{1}\002\00399 successfully added to emote list!".format(word[3], word[2]))
-        elif cmd == "delete" and len(word) == 3:
-            if word[2] in emote_dict:
-                del emote_dict[word[2]]
-                hexchat.prnt("*\t\00320\002{0}\002\00399 is no longer associated with an emote.".format(word[2]))
-            else:
-                hexchat.prnt("*\tThere is no emote associated with that alias!")
-        elif cmd == "emotes":
-            emote_lst = []
-            for key in emote_dict:
-                emote_lst.append(emote_dict[key])
-            emote_lst.sort()
-            hexchat.prnt(" ".join(emote_lst))
-        elif cmd == "aliases":
-            alias_lst = []
-            for key in emote_dict:
-                alias_lst.append(key)
-            alias_lst.sort()
-            hexchat.prnt(" ".join(alias_lst))
-        elif cmd == "output" and len(word) == 3:
-            if word[2] in emote_dict:
-                hexchat.prnt("*\tEmote association for \00320\002{0}\002\00399 is: \00319\002{1}\002\00399".format(word[2], emote_dict[word[2]]))
-            else:
-                hexchat.prnt("*\tThere is no emote associated with that alias!")
-        # Uncomment elif block below when combining with TingPing's twitch.py script
-        # elif cmd == "commands":
-        #     for command in commands:
-        #         print command, 
-        else:
-            hexchat.command("help twitch")
-            return hexchat.EAT_ALL
-
-    else:
-        hexchat.command("help twitch")
-        return hexchat.EAT_ALL
     
 def emote_cb(word, word_eol, userdata):
-    global unedited
+    global moved
 
-    if is_twitch() and unedited and len(word) > 0:
-        unedited = False
-        split_words = word_eol[0].split(" ")
+    if moved:
+        return
 
-        for i in range(0, len(split_words)):
-            if split_words[i].lower() in emote_dict:
-                split_words[i] = emote_dict[split_words[i].lower()]
+    if is_twitch():
+        for w in word:
+            if w.lower() in emote_dict:
+                word[word.index(w)] = emote_dict[w.lower()]
 
-        new_words = " ".join(split_words)
-        hexchat.command("say {}".format(new_words))
+        new_msg = " ".join(word)
+
+        moved = True
+        hexchat.command("SAY {0}".format(new_msg))
+        moved = False
+
         return hexchat.EAT_ALL
 
-    else:
-        unedited = True
-        return hexchat.EAT_NONE
-
 hexchat.hook_command("", emote_cb, priority=hexchat.PRI_HIGH)
-hexchat.hook_command("twitch", twitch_cb, help=twitch_help)
+
+hexchat.prnt(__module_name__ + " version " + __module_version__ + " loaded")
