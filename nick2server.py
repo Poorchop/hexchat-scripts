ed5de6f01e5a37c63b0fd51c76a7cb23b03440b4
Zooback <poordog@openmailbox.org>
2014-02-23 13:25:41 -0500

    Update nick2server.py

--- nick2server.py
+++ nick2server.py
@@ -2,7 +2,7 @@ import hexchat
 
 __module_name__ = "Nick to Server Tab"
 __module_author__ = "PDog"
-__module_version__ = "0.0.2"
+__module_version__ = "0.1"
 __module_description__ = "Move nick change messages to the server tab"
 
 moved = False
@@ -11,17 +11,18 @@ def move_cb(word, word_eol, userdata):
 	global moved
 
 	if moved:
-		return
-	else:
-		for chan in hexchat.get_list("channels"):
-			if chan.type == 1 and chan.id == hexchat.get_prefs("id"):
-				network_context = chan.context
+                return
+                
+        for chan in hexchat.get_list("channels"):
+                if chan.type == 1 and chan.id == hexchat.get_prefs("id"):
+                        network_context = chan.context
 				
-		moved = True
-		network_context.emit_print("Change Nick", word[0], word[1])
-		moved = False
+        moved = True
+        network_context.emit_print("Change Nick", word[0], word[1])
+        moved = False
 		
-		return hexchat.EAT_ALL
+        return hexchat.EAT_ALL
 
 hexchat.hook_print("Change Nick", move_cb)
+
 hexchat.prnt(__module_name__ + " version " + __module_version__ + " loaded")
