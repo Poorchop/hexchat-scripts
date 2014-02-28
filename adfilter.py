817c57456ccc041209597ba92386b7a2b88fa0fe
Zooback <poordog@openmailbox.org>
2014-02-28 15:43:35 -0500

    General whitespace fixes

--- adfilter.py
+++ adfilter.py
@@ -12,16 +12,27 @@ channels = []
 # Customize the name of the tab to your liking
 tab_name = "(Ads)"
 
-bwi_regex          = re.compile("^(\[BWI\])\sType\s+\W[\w\\\[\]{}^`|-]+\s+to\sget\sthe\slist\sof\s+[\d,]+\s+files\s\([\d\.]+\s+[A-Z]+\)\.\s+Updated\son\s+[\d+-]+\s+[\d:]+\.?\s+Total\sSent\(channel\):\s+[\d,]+\s+\([\d\.]+\s+[A-Z]+(\))$")
-irssi_fserve_regex = re.compile("^(\(FServe Online\))\s+Note:\(Type\s+\W[\w\\\[\]{}^`|-]+\s+for\s+filelist\)\s+Trigger:\(/ctcp\s+.*?\)\s+On\s+FServe:\(.*?\)\s+Sends:\(")
-iterati_regex      = re.compile(".*?Type\s+\W[\w\\\[\]{}^`|-]+\s+to\sget\smy\slist(\s|\.)?.*?\s+(Upd|(C|c)re)ated\son\s+[SMTWF][a-z]{2}\s+[A-Z][a-z]+\s+[\d\s:]+(\.).*?")
-ns_fserve_regex    = re.compile("^(Type)\s+\W.*?for\smy\stiny\slist.*?[\d,]+\s+book(s)?\sadded\son\s+[\d\.]+\s+:\s+[\w\W]+")
-omenserve_regex    = re.compile(".*?Type:\s+\W[\w\\\[\]{}^`|-]+\s+For\sMy\sList\sOf:\s+[\d,]+\s+Files\s+.*?Slots:\s+\d+/\d+\s+.*?Queued:\s+\d+\s+.*?Speed:\s+[\d,]+cps\s+.*?Next:\s+\w+\s+.*?Served:\s+[\d,]+\s+.*?List:\s+[A-Z][a-z]+\s+\w+\s+.*?Search:\s+[A-Z]{2,3}\s+.*?Mode:\s+\w+\s+.*?$")
-os_limits_regex    = re.compile("^(\s+)?(Sent:)\s+.*?To:\s+.*?Total\s+Sent:\s+[\d,]+\s+Files.*?Yesterday:\s[\d,]+\s+Files.*?Today.*?:\s+[\d,]+\s+Files.*?OS-Limits\s+(v[\d\.]+)$")
+bwi_regex          = re.compile("^(\[BWI\])\sType\s+\W[\w\\\[\]{}^`|-]+\s+to\sget\sthe\slist\sof\s+[\d,]+\s+files\s\(["
+                                "\d\.]+\s+[A-Z]+\)\.\s+Updated\son\s+[\d+-]+\s+[\d:]+\.?\s+Total\sSent\(channel\):\s+["
+                                "\d,]+\s+\([\d\.]+\s+[A-Z]+(\))$")
+irssi_fserve_regex = re.compile("^(\(FServe Online\))\s+Note:\(Type\s+\W[\w\\\[\]{}^`|-]+\s+for\s+filelist\)\s+Trigger:"
+                                "\(/ctcp\s+.*?\)\s+On\s+FServe:\(.*?\)\s+Sends:\(")
+iterati_regex      = re.compile(".*?Type\s+\W[\w\\\[\]{}^`|-]+\s+to\sget\smy\slist(\s|\.)?.*?\s+(Upd|(C|c)re)ated\son\s"
+                                "+[SMTWF][a-z]{2}\s+[A-Z][a-z]+\s+[\d\s:]+(\.).*?")
+ns_fserve_regex    = re.compile("^(Type)\s+\W.*?for\smy\stiny\slist.*?[\d,]+\s+book(s)?\sadded\son\s+[\d\.]+\s+:\s+[\w"
+                                "\W]+")
+omenserve_regex    = re.compile(".*?Type:\s+\W[\w\\\[\]{}^`|-]+\s+For\sMy\sList\sOf:\s+[\d,]+\s+Files\s+.*?Slots:\s+\d+"
+                                "/\d+\s+.*?Queued:\s+\d+\s+.*?Speed:\s+[\d,]+cps\s+.*?Next:\s+\w+\s+.*?Served:\s+[\d,]+"
+                                "\s+.*?List:\s+[A-Z][a-z]+\s+\w+\s+.*?Search:\s+[A-Z]{2,3}\s+.*?Mode:\s+\w+\s+.*?$")
+os_limits_regex    = re.compile("^(\s+)?(Sent:)\s+.*?To:\s+.*?Total\s+Sent:\s+[\d,]+\s+Files.*?Yesterday:\s[\d,]+\s+Fil"
+                                "es.*?Today.*?:\s+[\d,]+\s+Files.*?OS-Limits\s+(v[\d\.]+)$")
 single_file_regex  = re.compile("^(\s+)?Type:\s+\W[\w\\\[\]{}^`|-]+.*?To\sGet\sThis\s+.*?(File|MP3)$")
-unknown_1_regex    = re.compile("^(Type)\s+\W[\w\\\[\]{}^`|-]+\s+for\smy\slist\sof\s+\([\d,]+\)\s+Ebooks\screated\son\s+[\d-]+\s+([\d:]+)$")
+unknown_1_regex    = re.compile("^(Type)\s+\W[\w\\\[\]{}^`|-]+\s+for\smy\slist\sof\s+\([\d,]+\)\s+Ebooks\screated\son\s"
+                                "+[\d-]+\s+([\d:]+)$")
 unknown_2_regex    = re.compile("^(\s*((\(\)\(\)\()|(<><><)))\s+.*?\s+(((\)\(\)\(\))|(><><>))\s*)$")
-unknown_3_regex    = re.compile(".*?For\sMy\sList.*?\([\w:]+\)\sand\sDCC\sStatus,\stype\s[@\w\\\[\]{}^`|-]+\sand\s[@\w\\\[\]{}^`|-]+\.\s.*?Slots.*?Ques\sTaken.*?Next\sSend:.*?CPS\sin\sUse:.*?Highest\sCps\sRecord:.*?Total\sFile\sServed:.*?")
+unknown_3_regex    = re.compile(".*?For\sMy\sList.*?\([\w:]+\)\sand\sDCC\sStatus,\stype\s[@\w\\\[\]{}^`|-]+\sand\s[@\w"
+                                "\\\[\]{}^`|-]+\.\s.*?Slots.*?Ques\sTaken.*?Next\sSend:.*?CPS\sin\sUse:.*?Highest\sCps"
+                                "\sRecord:.*?Total\sFile\sServed:.*?")
 
 ad_lst = [bwi_regex, irssi_fserve_regex, iterati_regex, ns_fserve_regex,
           omenserve_regex, os_limits_regex, single_file_regex, unknown_1_regex,
@@ -31,50 +42,50 @@ server_nicks = []
 moved = False
 
 def find_adtab():
-	context = hexchat.find_context(channel=tab_name)
-	if context == None:
-		hexchat.command("NEWSERVER -noconnect {0}".format(tab_name))
-		return hexchat.find_context(channel=tab_name)
-	else:
-		return context
+    context = hexchat.find_context(channel=tab_name)
+    if context == None:
+        hexchat.command("NEWSERVER -noconnect {0}".format(tab_name))
+        return hexchat.find_context(channel=tab_name)
+    else:
+        return context
 		
 def adfilter_cb(word, word_eol, userdata):
-        word = [(word[i] if len(word) > i else "") for i in range(4)]
+    word = [(word[i] if len(word) > i else "") for i in range(4)]
         
-	global server_nicks
-	channel = hexchat.get_info("channel")
-	stripped_msg = hexchat.strip(word[1], -1, 3)
+    global server_nicks
+    channel = hexchat.get_info("channel")
+    stripped_msg = hexchat.strip(word[1], -1, 3)
 	
-	for ad in ad_lst:
-		if ad.match(stripped_msg) and channel in channels:		
-			if word[0] not in server_nicks:
-				server_nicks.append(word[0])
+    for ad in ad_lst:
+        if ad.match(stripped_msg) and channel in channels:
+            if word[0] not in server_nicks:
+                server_nicks.append(word[0])
 				
-			ad_context = find_adtab()
-			ad_context.prnt("{0}\t\00318<{4}{3}{1}>\00399 {2}".format(channel, *word))
-			return hexchat.EAT_ALL
+            ad_context = find_adtab()
+            ad_context.prnt("{0}\t\00318<{4}{3}{1}>\00399 {2}".format(channel, *word))
+            return hexchat.EAT_ALL
 
 def ctcpfilter_cb(word, word_eol, userdata):
-	global moved
+    global moved
 	
-	if moved:
-		return
+    if moved:
+        return
 	
-	if word[0][:5] == "SLOTS" or word[0][:3] == "MP3" or word[1] in server_nicks and word[2] in channels:
-		ad_context = find_adtab()
+    if word[0][:5] == "SLOTS" or word[0][:3] == "MP3" or word[1] in server_nicks and word[2] in channels:
+        ad_context = find_adtab()
 		
-		moved = True
-		ad_context.emit_print("CTCP Generic to Channel", *word)
-		moved = False
+        moved = True
+        ad_context.emit_print("CTCP Generic to Channel", *word)
+        moved = False
 		
-		return hexchat.EAT_ALL
+        return hexchat.EAT_ALL
 
 def unload_cb(userdata):
-	for chan in hexchat.get_list("channels"):
-		if chan.type == 1 and chan.channel == tab_name:
-			ad_context = hexchat.find_context(channel=tab_name)
-			ad_context.command("CLOSE")
-	hexchat.prnt(__module_name__ + " version " + __module_version__ + " unloaded")
+    for chan in hexchat.get_list("channels"):
+        if chan.type == 1 and chan.channel == tab_name:
+            ad_context = hexchat.find_context(channel=tab_name)
+            ad_context.command("CLOSE")
+    hexchat.prnt(__module_name__ + " version " + __module_version__ + " unloaded")
 
 hexchat.hook_print("Channel Message", adfilter_cb, priority=hexchat.PRI_HIGH)
 hexchat.hook_print("CTCP Generic to Channel", ctcpfilter_cb)
--- requestfilter.py
+++ requestfilter.py
@@ -3,7 +3,7 @@ import hexchat
 
 __module_name__ = "RequestFilter"
 __module_author__ = "PDog"
-__module_version__ = "0.2"
+__module_version__ = "0.3"
 __module_description__ = "Move search and file requests to a separate tab"
 
 # Add channels from which you would like to filter requests, e.g. channels = ["#freenode", "#defocus", "##linux"]
@@ -13,45 +13,34 @@ channels = []
 tab_name = "(Requests)"
 
 request_regex = re.compile("^![\w\\\[\]{}^`|-]+.*?\.([\w\d]{3,4}(\s+)?)$")
-search_words = ["@find", "@new", "@search", "@seek"]
-search_nicks = []
-
-def build_search_nicks():
-        global search_nicks
-
-        for user in hexchat.get_list("users"):
-                if "@" + user.nick not in search_nicks:
-                        search_nicks.append("@" + user.nick)
+search_regex = re.compile("^@[\w\\\[\]{}^`|-]+")
 
 def find_requesttab():
-        context = hexchat.find_context(channel=tab_name)
-        if context == None:
-                hexchat.command("NEWSERVER -noconnect {0}".format(tab_name))
-                return hexchat.find_context(channel=tab_name)
-        else:
-                return context
+    context = hexchat.find_context(channel=tab_name)
+    if context == None:
+        hexchat.command("NEWSERVER -noconnect {0}".format(tab_name))
+        return hexchat.find_context(channel=tab_name)
+    else:
+        return context
 
 def requestfilter_cb(word, word_eol, userdata):
-        word = [(word[i] if len(word) > i else "") for i in range(4)]
+    word = [(word[i] if len(word) > i else "") for i in range(4)]
 
-        split_words = word[1].split(" ")
-        channel = hexchat.get_info("channel")
+    channel = hexchat.get_info("channel")
 	
-        if channel in channels:	
-                build_search_nicks()
-	
-                if request_regex.match(word[1]) or split_words[0].lower() in search_words or split_words[0].split("-")[0] in search_nicks:
-                        request_context = find_requesttab()
-                        request_context.prnt("{0}\t\00318<{4}{3}{1}>\00399 {2}".format(channel, *word))
+    if channel in channels:
+        if request_regex.match(word[1]) or search_regex.match(word[1]):
+            request_context = find_requesttab()
+            request_context.prnt("{0}\t\00318<{4}{3}{1}>\00399 {2}".format(channel, *word))
 
-                        return hexchat.EAT_ALL
+            return hexchat.EAT_ALL
 		
 def unload_cb(userdata):
-        for chan in hexchat.get_list("channels"):
-                if chan.type == 1 and chan.channel == tab_name:
-                        ad_context = hexchat.find_context(channel=tab_name)
-                        ad_context.command("CLOSE")
-        hexchat.prnt(__module_name__ + " version " + __module_version__ + " unloaded")
+    for chan in hexchat.get_list("channels"):
+        if chan.type == 1 and chan.channel == tab_name:
+            ad_context = hexchat.find_context(channel=tab_name)
+            ad_context.command("CLOSE")
+    hexchat.prnt(__module_name__ + " version " + __module_version__ + " unloaded")
 	
 hexchat.hook_print("Channel Message", requestfilter_cb, priority=hexchat.PRI_HIGH)
 hexchat.hook_unload(unload_cb)
